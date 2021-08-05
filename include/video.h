d5ad88b16e95491e3b512b16090607bf74a05622
Lucas Sousa <ls442336@gmail.com>
2021-08-05 17:51:59 -0300

    Setup testing with gtest

--- include/video.h
+++ include/video.h
@@ -14,6 +14,7 @@ private:
 public:
     Video();
     void ClearBuffer();
+    unsigned char* GetBuffer();
 };
 
 }
--- src/video.cpp
+++ src/video.cpp
@@ -11,4 +11,9 @@ void Video::ClearBuffer()
     std::memset(buffer_, 0, sizeof(buffer_));
 }
 
+unsigned char* Video::GetBuffer()
+{
+	return buffer_;
+}
+
 }
